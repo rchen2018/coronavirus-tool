8099ced49519cdd2f148f01e3f4a44f129db88f9
Robert Chen <rec43@duke.edu>
2020-04-20 00:58:46 -0400

    cleaning up code and adding finishing touches

--- src/Components/Ending.js
+++ src/Components/Ending.js
@@ -1,48 +1,44 @@
 import React, { Component } from "react";
-import { Header } from "semantic-ui-react";
-import { List } from "semantic-ui-react";
 import { Image } from "semantic-ui-react";
 import Logo from "../badges.png";
 
 class Ending extends Component {
   render() {
     return (
-      <div class="end">
-        <div class="tips">
-          <h1>Here are some tips on reducing usage</h1>
-          <ul>
-            <li>Unplug your devices when not in use</li>
-            <li>Switch to LED lightbulbs </li>
-            <li>Switch to energy efficient appliances </li>
-            <li>Invest in a smart thermostat </li>
-            <li>Take shorter showers</li>
-          </ul>
-        </div>
-
-        <div class="facts">
-          <h1>Everything Counts </h1>
+      <div id="end">
+        <div id="tips">
+          <h1>How to Stay Healthy</h1>
           <ul>
             <li>
-              On average 10 gallons of your water footprint is lost daily due to
-              leaks
+              Wash your hands often with soap and water for at least 20 seconds
             </li>
             <li>
-              You can save 15 gallons of water in 10 minutes by switching to a
-              low-flow showerhead
+              Avoid touching your eyes, nose, and mouth with unwashed hands
             </li>
-            <li>2.5 billion people lack reliable access to electricity</li>
-            <li>2.8 billion people live in areas of high water scarcity</li>
+            <li>
+              Practice social distancing to avoid contact with people who are
+              sick
+            </li>
+            <li>
+              Cover your mouth and nose with a cloth face cover when around
+              others
+            </li>
+            <li>Clean and disinfect frequently touched surfaces daily</li>
           </ul>
+          <br />
+          <br />
+          <a
+            href="https://www.cdc.gov/coronavirus/2019-ncov/prevent-getting-sick/prevention.html"
+            rel="noopener noreferrer"
+            target="_blank"
+            id="CDC_link"
+          >
+            Learn More
+          </a>
         </div>
 
-        <div class="badges">
-          <Image src={Logo} alt="Badge Collection" size="huge" centered />
-          <h2>Almost there! Earn 2 more badges to unlock more rewards!</h2>
-          <br />
-          <br />
-          <h2>The Green Path plants a tree for every badge that you earn :)</h2>
-          <br />
-          <h2> Earn 5 badges to qualify for a rebate!</h2>
+        <div id="created_by">
+          <h3>Created by Robert Chen and Ahmad Khan</h3>
         </div>
       </div>
     );
